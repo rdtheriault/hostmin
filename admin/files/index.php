90ab8fe0797f60d2a76f5939aca6a65b0c487fd6
Robert <rdtheriault@yahoo.com>
2024-03-20 13:37:01 -0700

    Update index.php
    updated from testUser

--- admin/files/index.php
+++ admin/files/index.php
@@ -15,20 +15,39 @@ if (strtolower($_SESSION['user']) != strtolower($folders[2])){
 // Get the directory to list (default to the current directory)
 if (isset($_POST['fileName'])){
 	if ($_POST['fileName'] == ".."){
-		$directory = "../";
+		//get rid of last folder
+		$parts = explode('/',$_SESSION['path']);
+		$new = "";
+		for ($i = 1; $i < count($parts)-1; $i++){
+			$new .= '/'.$parts[$i];
+		}
+		$directory = "..".$new;
+		//check if root and fix
+		if ($directory == '..'){
+			$directory = "../";
+		}
+	}else if ($_POST['fileName'] == "/////"){//use same path for creating a file/folder
+		$directory =  $_SESSION['path'];
 	}else{
-		$directory =  $_POST['fileName'];
+		if ($_SESSION['path'] == "../"){
+			$directory =  $_SESSION['path'].$_POST['fileName'];
+		}else{
+			$directory =  $_SESSION['path'].'/'.$_POST['fileName'];
+		}
 	}
+	
 }
 else{
 	$directory = "../";
 }
+$_SESSION['path'] = $directory;
+
 $folder = str_replace("../", "", $directory);
 if ($folder != ""){
 	$folder .= "/";
 }
 // Handle form submission to create a file or folder
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['name']) && isset($_POST['type'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['name']) && isset($_POST['type']) && !isset($_POST['pic'])) {
     $name = $_POST['name'];
     $type = $_POST['type'];
 	//$folder = $_POST['path'];
@@ -40,7 +59,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['name']) && isset($_POS
 
         // Determine the path to create the file or folder
         $path = $directory . '/' . $name;
-		echo $path;
+
         // Check if the file or folder already exists
         if ($type === 'file' && !file_exists($path)) {
             // Create the file
@@ -50,6 +69,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['name']) && isset($_POS
             mkdir($path);
         } else {
             echo "File or folder already exists!";
+        }
+    } else {
+        echo "Name cannot be empty!";
+    }
+}
+// Handle form submission to upload pic
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['pic'])) {
+
+    // Check if the name is not empty
+    if ( $_POST['pic'] == "yes") {
+		$uploadedFile = $_FILES["file"];
+		
+        // Determine the path to create the file or folder
+        $path = $directory.basename($uploadedFile["name"]);
+		
+        // Check if the file or folder already exists
+        if (!file_exists($path)) {
+            // Check if uploaded file is an image
+            if ($_FILES['file']['error'] === UPLOAD_ERR_OK && getimagesize($_FILES['file']['tmp_name']) !== false) {
+                // Move the uploaded file to the destination folder
+                move_uploaded_file($_FILES['file']['tmp_name'], $path);
+            } else {
+                echo "File upload failed. Please upload only images.";
+            }
+		}else {
+            echo "File already exists!";
         }
     } else {
         echo "Name cannot be empty!";
@@ -72,19 +117,35 @@ function listFilesAndFolders($dir, $folder) {
 							echo "<div class='file' onclick=\"postFolderName('..')\">$item</div><br>";
 						}
 						else if (is_dir("$dir/$item")) {
-							echo "<div class='file' onclick=\"postFolderName('../$item')\">$item</div><br>";
+							echo "<div class='file' onclick=\"postFolderName('$item')\">$item (Folder)</div><br>";
 						} else {
-							// If it's a file, make the name clickable and post its name to another page
-							echo "<div class='file' onclick=\"postFileName('$folder$item')\">$item</div><br>";
+							// Check if the file is a picture
+							$extension = strtolower(pathinfo($item, PATHINFO_EXTENSION));
+							$pictureExtensions = array('jpg', 'jpeg', 'png', 'gif');
+							if (!in_array($extension, $pictureExtensions)) {
+								// If it's not a picture, make it clickable
+								echo "<div class='file' onclick=\"postFileName('$folder$item')\">$item</div><br>";
+							} else {
+								// If it's a picture, just display the name
+								echo "<div>$item (Picture)</div><br>";
+							}
 						}
 					}
 					else if ($item != '..' ){
 						// If it's a directory, make it clickable
 						if (is_dir("$dir/$item")) {
-							echo "<div class='file' onclick=\"postFolderName('../$item')\">$item</div><br>";
+							echo "<div class='file' onclick=\"postFolderName('$item')\">$item (Folder)</div><br>";
 						} else {
-							// If it's a file, make the name clickable and post its name to another page
-							echo "<div class='file' onclick=\"postFileName('$folder$item')\">$item</div><br>";
+							// Check if the file is a picture
+							$extension = strtolower(pathinfo($item, PATHINFO_EXTENSION));
+							$pictureExtensions = array('jpg', 'jpeg', 'png', 'gif');
+							if (!in_array($extension, $pictureExtensions)) {
+								// If it's not a picture, make it clickable
+								echo "<div class='file' onclick=\"postFileName('$folder$item')\">$item</div><br>";
+							} else {
+								// If it's a picture, just display the name
+								echo "<div>$item (Picture)</div><br>";
+							}
 						}
 					}
                 }
@@ -143,12 +204,9 @@ function listFilesAndFolders($dir, $folder) {
 <body>
     <h2>File Explorer - <?php echo $directory; ?></h2>
     <?php
-    // List files and folders in the specified directory
-    listFilesAndFolders($directory, $folder);
+		// List files and folders in the specified directory
+		listFilesAndFolders($directory, $folder);
     ?>
-	
-	
-	
 	
 	<hr>
     <h3>Create File or Folder</h3>
@@ -159,9 +217,17 @@ function listFilesAndFolders($dir, $folder) {
             <option value="file">File</option>
             <option value="folder">Folder</option>
         </select>
-		<input type="hidden" value="<?php echo $directory; ?>" name="fileName">
+		<input type="hidden" value="/////" name="fileName">
         <input type="submit" value="Create">
     </form>
-	
+	<br><br>
+	<hr>
+    <h3>Upload Pic</h3>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>" enctype="multipart/form-data">
+		<input type="file" name="file" accept="image/*">
+		<input type="hidden" value="/////" name="fileName">
+		<input type="hidden" value="yes" name="pic">
+        <input type="submit" value="Upload">
+    </form>
 </body>
 </html>
